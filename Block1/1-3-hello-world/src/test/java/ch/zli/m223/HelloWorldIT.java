477904be0edd481f8a99f3d37597d11891af9b0a
Oliver Zenger <oliver@zenger.org>
2024-11-04 08:37:28 +0000

    chore: scaffold project

---
+++ Block1/1-3-hello-world/src/test/java/ch/zli/m223/HelloWorldIT.java
@@ -0,0 +1,8 @@
+package ch.zli.m223;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class HelloWorldIT extends HelloWorldTest {
+    // Execute the same tests but in packaged mode.
+}
